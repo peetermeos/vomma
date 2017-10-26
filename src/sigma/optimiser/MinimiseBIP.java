5e7752e70539a928183d35abe368f98753f7dec6
Peeter Meos <peeterm@proekspert.ee>
2017-10-26 14:21:32 +0300

    Fix

--- src/sigma/optimiser/MinimiseBIP.java
+++ src/sigma/optimiser/MinimiseBIP.java
@@ -1,46 +1,5 @@
 package sigma.optimiser;
 
-import com.joptimizer.exception.JOptimizerException;
-import com.joptimizer.optimizers.BIPLokbaTableMethod;
-import com.joptimizer.optimizers.BIPOptimizationRequest;
-
-/**
- * General BIP minimisation
- * 
- * @author Peeter Meos
- * @version 0.2
- *
- */
-public class MinimiseBIP {
-
-	BIPLokbaTableMethod opt;
-	BIPOptimizationRequest or;
-	
-	
-	public void setup() {
-		
-	}
-	
-	public void optimise() {
-		or = new BIPOptimizationRequest();
-		
-		//	optimization
-		opt = new BIPLokbaTableMethod();
-		opt.setBIPOptimizationRequest(or);
-		try {
-			opt.optimize();
-		} catch (JOptimizerException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void results() {
-		
-	}
-}
-package sigma.optimiser;
-
 import java.util.ArrayList;
 
 import org.apache.commons.lang3.ArrayUtils;
